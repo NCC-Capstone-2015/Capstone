51117dc0b585875b3a50a76cc5a21670249cd25f
Andrew Bockus <agbockus@gmail.com>
2015-05-13 14:26:51 -0500

    Start of Reports view functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :welcomes
   resources :user_informations
   get 'welcomes/index'
+  post '/reports/show' => 'reports#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
