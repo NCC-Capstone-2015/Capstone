d0792b84898907449ca8430ba2eb5a28689c5563
Andrew Bockus <agbockus@gmail.com>
2015-05-27 23:46:58 -0500

    Removed unneeded folders and files for reports. Created other report partials. Removed dropdown option for Surveys link on navigation bar (since there were no options).
    Corey fixed a small issue in seeds.rb
    Removed leftover test code in /user_informations/show.html.erb

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,16 +5,41 @@ class ReportsController < ApplicationController
   end
 
   def display
+    @report_type = report_params
+
+    case @report_type
+    when "date"
+
+    when "grad_class"
+
+    when "grad_program"
+
+    when "employer"
+
+    when "taken_surveys"
+
+    when "given_back"
+
+    when "survey_completion"
+
+    when "survey_results"
+
+    when "user_survey_response"
+
+    else
+      redirect_to reports_path
+    end
   end
 
   def options
     # Get parameters in order to render correct form
     @report_type = report_params
+    @surveys = Survey.all
 
-    if @report_type == ""
+    if @report_type.nil?
       redirect_to reports_path
     end
-    
+
     # Get array of saved lists to populate dropdown from collection
     # @saved_list_array = Login.find(session[:id]).saved_lists.all
     @saved_list_array = Login.find(1).saved_lists.all
