5fcc6e6cf735683cebf0d4fb2ac767e1188cadc4
Andrew Fredrickson <andymfred@gmail.com>
2015-06-06 21:48:54 -0500

    Added authentication for user/admin on each page. Added login type based menu loading.

--- app/controllers/admin_auth_controller.rb
+++ app/controllers/admin_auth_controller.rb
@@ -1,11 +1,11 @@
 class AdminAuthController < AuthenticationController
   before_filter :authorized?
-  
+
   private
   def authorized?
-    unless current_login.login_type == 1 || current_login.login_type == 2
-      flash[:error] = "unauthorized access: redirected to sign in"
-      redirect_to root_path
+    unless current_login.login_type == 1 || current_login.login_type == 0
+      flash[:error] = "unauthorized access: redirected to homepage"
+       redirect_to root_path
     end
   end
-end 
+end
