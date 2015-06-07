e761cb2c86d3c3899eca5c246264794b2b5b7efa
JTMaher2 <jtmaher2@gmail.com>
2015-06-07 00:42:07 -0500

    saved lists display users

--- app/controllers/saved_lists_controller.rb
+++ app/controllers/saved_lists_controller.rb
@@ -6,5 +6,7 @@ class SavedListsController < ApplicationController
   
   def show
     @saved_list = SavedList.find(params[:id])
+    @user = User.find(SavedListUser.find(@saved_list.login_id).user_id)
+    @login = Login.find(@user.login_id)
   end
 end
