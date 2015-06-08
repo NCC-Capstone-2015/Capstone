0f791dd047ef393a58eb4b55d56db992fe6b111e
Andrew Bockus <agbockus@gmail.com>
2015-06-08 14:03:51 -0500

    Complete styling of all pages after setting background image Worked on Saved Lists

--- app/controllers/saved_lists_controller.rb
+++ app/controllers/saved_lists_controller.rb
@@ -1,23 +1,40 @@
-# James Maher
+# James Maher and Andrew Bockus
 class SavedListsController < ApplicationController
 
   def index
     @saved_lists = Login.find(current_login.id).saved_lists.all
   end
-  
+
   def show
     @saved_list = SavedList.find(params[:id])
     @user = User.find(SavedListUser.find(@saved_list.login_id).user_id)
     @login = Login.find(@user.login_id)
   end
-  
+
   def new
-    @saved_list = SavedList.find(params[:id])
+    @saved_list = SavedList.new
+    @saved_list_array = SavedList.find(current_login.id).saved_lists.all
   end
-  
-  def delete_user
-    @saved_list = SavedList.find(params[:id])
-    @saved_list_user = SavedListUser.find(@saved_list.login_id)
+
+  def create
+    @users_array = params[:saved_list_array]
+    @saved_list = SavedList.new
+
+    @users_array.each do |user|
+      @saved_list.saved_list_user.new
+      @saved_list.saved_list_user.user_id = user.id
+    end
+  end
+
+  # Delete user method
+  def delete_user(user_id, list_id)
+    @user_to_delete = SavedList.find(list_id).saved_list_user.find(user_id)
     SavedListUser.destroy(@user.user_id)
+  end
+
+  # Delete saved list method
+  def delete
+    @saved_list = SavedList.find(params[:id])
+    @saved_list.destroy
   end
 end
